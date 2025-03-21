266528e59448c74312968faefb0cac2caa63af67
programmingsquid <avaandelijah22@gmail.com>
2025-03-21 10:39:21 -0500

    README.md and logger.mjs updated

---
+++ README.md
@@ -0,0 +1,6 @@
+before running (on server):
+npm install node-fetch dotenv winston
+
+dotenv - reads key-value pairs and can set them as enviornment cariables https://www.bing.com/ck/a?!&&p=c3dae8c6e3b8fa4f40284490b3fc1156bc2af7a572b2bc7d5b39a37736be8c84JmltdHM9MTc0MjUxNTIwMA&ptn=3&ver=2&hsh=4&fclid=0401e12b-f3e2-6219-09aa-f4bdf7e260d3&psq=dotenv&u=a1aHR0cHM6Ly93d3cuZG90ZW52Lm9yZy9kb2NzLw&ntb=1
+node fetch - figure this out later
+winston - logging library for Node.js like errors and shit
---
+++ src/utils/logger.mjs
@@ -0,0 +1,21 @@
+import winston from 'winston';
+
+//figure out what this does lmao
+const logger = winston.creatLooger({
+    level: 'info', //change to error, warn, debug?
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+),
+//add definition to README.md
+transports: [
+    new winston.transports.Console(),
+
+//will need this later once implemented
+//Add to README.md
+//new winston.transport.File({ filename: 'logs/app.log' })
+        ]
+    }
+);
+//makes the logger object usable in other parts of the application
+export default logger;

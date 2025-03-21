b1baf660f8cccea5bb278b5396e3067b95e4d9da
programmingsquid <avaandelijah22@gmail.com>
2025-03-21 13:15:27 -0500

    .env overhaul

---
+++ src/config/env.mjs
@@ -0,0 +1,9 @@
+//make sure to put correct numbers and ID credintials
+    //whats this do?
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const LIGHTSPEED_CLIENT_ID = process.env.LIGHTSPEED_CLIENT_ID;
+export const LIGHTSPEED_CLIENT_SECRET = process.env.LIGHTSPEED_CLIENT_SECRET;
+export const LIGHTSPEED_REFRESH_TOKEN = process.env.LIGHTSPEED_REFRESH_TOKEN;
+export const LIGHTSPEED_ACCOUNT_ID = process.env.LIGHTSPEED_ACCOUNT_ID;
